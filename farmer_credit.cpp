a83222ba3b972e329caa600ce3bd33af859582b2
jeronimo <jerohincapie@gmail.com>
2026-05-29 23:53:05 -0500

    base

---
+++ farmer_credit.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+//declarar funcion
+int chechCredits(int a);
+
+int main()
+{
+    int horse = 10;
+    int pig = 3;
+    float rabbit = 0.5;
+    float farmer_credit = 100.0;
+}
